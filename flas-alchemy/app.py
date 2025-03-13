271d26155036d2a178e3add6531672435d751fc7
Cesar Martinez <cesarwamartinez@gmail.com>
2025-03-13 17:29:52 -0500

    Add cors

--- flas-alchemy/app.py
+++ flas-alchemy/app.py
@@ -6,24 +6,28 @@ from routes.productos import productos
 from routes.categorias import categorias
 from routes.auth import auth_bp
 from flask_cors import CORS
-
 from flask import Flask, request, jsonify
 from utils.respuestas import respuesta_fail, respuesta_success
 import uuid
 
 app = Flask(__name__)
 
+# Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/alchemy_flask_db'
-CORS(app, resources={r"/categorias": {"origins": "http://localhost:5173", "methods":["GET", "POST", "PUT", "DELETE"], "allow_headers": ["Content-Type", "Authorization"]}})
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
-if not os .path.exists(app.config['UPLOAD_FOLDER']):
+# Habilitar CORS globalmente
+CORS(app, resources={r"/*": {
+    "origins": "http://localhost:5173", 
+    "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    "allow_headers": ["Content-Type", "Authorization"]
+}})
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
-
-
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
@@ -43,8 +47,10 @@ def upload_file():
 
     return jsonify({"url": image_url}), 201
 
+# Inicializar la base de datos
 db.init_app(app)
 
+# Registrar las rutas
 app.register_blueprint(pedidos)
 app.register_blueprint(productos)
 app.register_blueprint(categorias)
