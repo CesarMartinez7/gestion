f1b7a125e8ff032097ee5b32dd6a2f3eeeca08e5
Cesar Martinez <cesarwamartinez@gmail.com>
2025-03-25 08:43:21 -0500

    SI

--- src/routes/user-registrer.tsx
+++ src/routes/user-registrer.tsx
@@ -10,6 +10,10 @@ interface ResponseToken {
   success: boolean;
 }
 
+
+
+
+
 export default function UserRegistrer() {
   const inputRefUserName = useRef<HTMLInputElement | null>(null);
   const inputRefUserPassword = useRef<HTMLInputElement | null>(null);
@@ -18,16 +22,18 @@ export default function UserRegistrer() {
   const [isBad, setIsBad] = useState<boolean>(false);
 
 
-  const {theme} = useThemeStore()
-  
 
-  const [ token,setToken] = useState<string>("");
+  const { theme } = useThemeStore()
 
-  
+
+  const [token, setToken] = useState<string>("");
+
+
+
 
   const handleSubmitLogin = (e: React.FormEvent, username: string, password: string) => {
     e.preventDefault();
-    console.log("Registro aquí...");
+
 
     fetch("http://127.0.0.1:5000/login", {
       method: "POST",
@@ -47,7 +53,6 @@ export default function UserRegistrer() {
         if (!data) {
           throw new Error("Ocurrió un error al obtener el token");
         }
-        console.log(data.data, data);
         setToken(data.data);
         localStorage.setItem("token", data.data);
         setIsLogeado(true);
@@ -85,20 +90,20 @@ export default function UserRegistrer() {
           <label className="fieldset-label">Contraseña</label>
           <input
             type="password"
-            className="input"
+            className="input "
             ref={inputRefUserPassword}
             defaultValue={"admin"}
             placeholder="Password"
           />
           <p className="text-red-500">{isBad ? "Verifica tu usuario o contraseña 🚀❌" : ""}</p>
 
-          <button className="btn btn-neutral mt-4">Iniciar Sesión</button>
-          <button className="btn btn-xs" onClick={() => localStorage.clear()}>
+          <button className="btn btn-neutral mt-4 ">Iniciar Sesión</button>
+          <button className="btn btn-xs " onClick={() => localStorage.removeItem("token")}>
             Cerrar sesión
           </button>
         </fieldset>
       </form>
-        
+
     </main>
   );
 }
